6b4ab136e19769edae4af681bb993bad0aa60742
Amy Gill <amy.gill.34@gmail.com>
2018-04-17 14:35:58 -0400

    getting set up

---
+++ mySQLcommands.sql
@@ -0,0 +1,9 @@
+
+use pokemon;
+SELECT * from types;
+
+
+
+
+create table trainers(trainerID int not null primary key,trainername text null);
+ALTER TABLE pokemon.trainers MODIFY COLUMN trainername VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
