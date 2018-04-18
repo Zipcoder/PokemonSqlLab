c54e18ba21de5929befd1f513419c65e26352c89
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-04-18 08:27:53 -0400

    working on part3

--- pokemon_sql/part3.sql
+++ pokemon_sql/part3.sql
@@ -13,7 +13,14 @@ FROM pokemon.pokemons
 WHERE name='Rufflet'
 
 -- What are the names of the pokemon that belong to the trainer with trainerID 303?
+SELECT name
+FROM pokemon.pokemons
+JOIN pokemon.pokemon_trainer ON pokemon_id = id
+WHERE trainerID = 303
 -- How many pokemon have a secondary type Poison
+ SELECT COUNT(secondary_type)
+FROM pokemon.pokemons
+WHERE secondary_type = 7
 -- What are all the primary types and how many pokemon have that type?
 -- How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer
 -- How many pokemon only belong to one trainer and no other?
