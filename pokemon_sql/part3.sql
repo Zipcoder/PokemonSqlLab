b70803b8207fe8fcfed7e055a3d565aa46779828
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-04-18 08:58:50 -0400

    saving

--- pokemon_sql/part3.sql
+++ pokemon_sql/part3.sql
@@ -26,4 +26,9 @@ SELECT pokemons.name , COUNT(pokemons.id)
 FROM pokemon.types JOIN pokemon.pokemons ON primary_type =pokemons.id
 GROUP BY pokemons.name
 -- How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer
+SELECT trainerID, COUNT(pokelevel) AS 'level_100_pokemon'
+FROM pokemon.pokemon_trainer
+WHERE pokelevel = 100
+GROUP BY trainerID;
+
 -- How many pokemon only belong to one trainer and no other?
