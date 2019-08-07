f342594c587a4e9dfbe515dadab97e0bdab66a47
Joanna Hsiao <chsiao58@gmail.com>
2019-08-07 17:28:11 -0400

    solution

---
+++ Joins and Groups.sql
@@ -0,0 +1,20 @@
+# What is each pokemon's primary type?
+select pokemons.name, types.name from pokemons join types on pokemons.primary_type = types.id;
+# What is Rufflet's secondary type?
+select types.name from pokemons join types on pokemons.secondary_type = types.id where pokemons.name = "Rufflet";
+# What are the names of the pokemon that belong to the trainer with trainerID 303?pokemon_trainertrainers
+select name from pokemons join pokemon_trainer on pokemons.id = pokemon_trainer.pokemon_id where trainerId = 303;
+# How many pokemon have a secondary type Poison
+select count(*) from pokemons join types on pokemons.secondary_type = types.id where types.name = "Poison";
+# What are all the primary types and how many pokemon have that type?
+select types.name, count(*) from pokemons join types on pokemons.primary_type = types.id group by types.name;
+# How many pokemon at level 100 does each trainer with at least one level 100 pokemone have?
+select trainers.trainerId, count(*) from 
+pokemon_trainer join trainers
+on pokemon_trainer.trainerID = trainers.trainerId
+where pokelevel = 100
+group by trainers.trainerId;
+# How many pokemon only belong to one trainer and no other?
+select count(*) as total from (
+	select count(*) as amount from pokemon_trainer 
+    group by pokemon_id having amount = 1) as p;
