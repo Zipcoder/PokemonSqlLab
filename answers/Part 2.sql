97607c7e3a78f6f14071017d4a9452500b0080b1
jgiroso <jrgiroso@gmail.com>
2021-07-29 21:49:11 -0400

    part 2 complete

---
+++ answers/Part 2.sql
@@ -0,0 +1,19 @@
+# Question: What are all the types of pokemon that a pokemon can have?
+# Answer: Normal, Water, Grass, Rock, Fire, Ground, Poison, Bug, Electric, Dragon, Steel, Dark, Fighting, Psychic, Ghost, Fairy, Ice, Flying
+SELECT name FROM types;
+
+# Question: What is the name of the pokemon with id 45?
+# Answer: Eevee
+SELECT name FROM pokemons WHERE id = 45;
+
+# Question: How many pokemon are there?
+# Answer: 656
+SELECT COUNT(*) FROM pokemons;
+
+# Question: How many types are there?
+# Answer: 18
+SELECT COUNT(*) FROM types;
+
+# Question: How many pokemon have a secondary type?
+# Answer: 316
+SELECT COUNT(secondary_type) FROM pokemons;
