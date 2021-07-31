93052f3333e77d4584efc94a7378109b565bce63
jgiroso <jrgiroso@gmail.com>
2021-07-30 22:18:46 -0400

    lab complete

--- answers/Part 3.sql
+++ answers/Part 3.sql
@@ -17,7 +17,8 @@ SELECT COUNT(secondary_type) FROM pokemons p JOIN types t ON p.secondary_type  =
 SELECT t.name, COUNT(primary_type) FROM pokemons p JOIN types t ON p.primary_type = t.id GROUP BY t.name;
 
 # Question: How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer
-SELECT COUNT(pokelevel) FROM pokemon_trainer WHERE pokelevel = 100 GROUP BY trainerID;
+SELECT DISTINCT(t.trainername), COUNT(pokelevel) FROM pokemon_trainer pt JOIN trainers t ON pt.trainerID = t.trainerID WHERE pokelevel = 100 GROUP BY pt.trainerID;
 
 # Question: How many pokemon only belong to one trainer and no other?
-# Answer: 
+# Answer: 13
+SELECT COUNT(pokemon_id), p.name FROM pokemon_trainer pt JOIN pokemons p ON pt.pokemon_id = p.id GROUP BY pokemon_id HAVING COUNT(pokemon_id) = 1;
